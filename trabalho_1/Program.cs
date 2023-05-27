9852bbee81f08813f5e04c0223ef8d4f0cc19df2
Wesley Santos <wesleysotnas64@gmail.com>
2023-05-27 16:14:49 -0300

    :sparkles: feat: Analisador léxico.

--- trabalho_1/Program.cs
+++ trabalho_1/Program.cs
@@ -29,7 +29,7 @@ using System.Collections.Generic;
                            -
 
 <multiplying operator> ::= * |
-                           div
+                           /
 
                 <sign> ::= + |
                            - |
@@ -49,10 +49,15 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        
-        string input = "999identifier10=<><<=>=>|&";
+        string test_01 = "idade:=(19<limite#)&#(limite>15)#";
+        string test_02 = "resultado:=((idade-2)+(2*(idade-3)))*((idade-10)/2.5)";
+        string test_03 = "id:=12+(1.9*4-5)/3(v1<17.1|id<>1)";
+
+        string input = test_03;
+
         LexicalAnalyzer.PreSelect(input);
         Utils.PrintTokens();
+        Utils.PrintHighlighted();
     }
 
     private static bool S(string input)
@@ -123,83 +128,87 @@ public class Program
     }
 }
 
-
-public static class Global{
-    public static string digits = "0123456789";
-    public static string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    public static string addingOperator = "+-";
-    public static string sign = "+-";
-    public static string multiplyingOperator = "*/";
-    public static string relational = "=<>|&";
-    public static char dot = '.';
-    public static List<Token> tokens = new List<Token>();
-}
-
-public enum TokenType
+public class LexicalIndicator
 {
-    Null = 0,
-    Identifier = 1,
-    Sign = 2,
-    MultiplyingOperator = 3,
-    AddingOperator = 4,
-    RelationalOperator = 5,
-    Int = 6,
-    Float = 7
-}
-
-public class Token
-{
-    public TokenType type;
-    public string value = "";
+    public int prox;
+    public bool error;
 }
 
 public static class LexicalAnalyzer
 {
     public static void PreSelect(string input)
     {
-        int i = 0;
-        while(i < input.Length)
+        LexicalIndicator li = new LexicalIndicator
         {
-            Console.WriteLine("Tamanho: {0} | i: {1}", input.Length, i);
+            prox = 0,
+            error = false
+        };
 
-            if(Utils.CharIsIn(Global.letters, input[i]))
+        while((li.prox < input.Length) && (li.error == false))
+        {
+            if(Utils.CharIsIn(Global.letters, input[li.prox]))
             {
-                i = IdentifierAutomaton(input, i);
+                li = IdentifierAutomaton(input, li.prox);
             }
-            else if(Utils.CharIsIn(Global.digits, input[i]))
+            else if(Utils.CharIsIn(Global.digits, input[li.prox]))
             {
-                i = NumbersAutomaton(input, i);
+                li = NumbersAutomaton(input, li.prox);
             }
-            else if(Utils.CharIsIn(Global.relational, input[i]))
+            else if(Utils.CharIsIn(Global.relational, input[li.prox]))
             {
-                i = RelationalAutomaton(input, i);
+                li = RelationalAutomaton(input, li.prox);
+            }
+            else if(Utils.CharIsIn(Global.parenthesis, input[li.prox]))
+            {
+                li = ParenthesisAutomaton(input, li.prox);
+            }
+            else if(Utils.CharIsIn(Global.addingOperator, input[li.prox]) ||
+                    Utils.CharIsIn(Global.multiplyingOperator, input[li.prox])
+                    )
+            {
+                li = OperatorAutomaton(input, li.prox);
+            }
+            else if(input[li.prox] == ':')
+            {
+                li = AssignmentAutomaton(input, li.prox);
             }
             else
             {
-                Console.WriteLine("ERRO LÉXICO! Lexema [{0}] posição[{1}] fora da tabela de símbolos.", input[i], i);
-                Console.WriteLine(input);
-                for(int j = 0; j < i+1; j++)
-                {
-                    if(j != i)
-                        Console.Write(" ");
-                    else
-                        Console.WriteLine("^");
-                }
-                break;
+                PresentLexicalError(input, li, "Símbolo não pertence a gramática.");
+                li.prox++;
+                // break;
             }
 
-            Console.WriteLine("Tamanho: {0} | i: {1}", input.Length, i);
+            if(li.error)
+            {
+                PresentLexicalError(input, li);
+                break;
+            }
         }
     }
 
-    public static int IdentifierAutomaton(string input, int i)
+    public static void PresentLexicalError(string input, LexicalIndicator li, string message = "")
     {
+        Console.WriteLine("ERRO LÉXICO! Lexema [{0}] posição [{1}]. \n{2}", input[li.prox], li.prox, message);
+        Console.WriteLine(input);
+        for(int j = 0; j < li.prox+1; j++)
+        {
+            if(j != li.prox)
+                Console.Write(" ");
+            else
+                Console.WriteLine("^");
+        }
+    }
+
+    public static LexicalIndicator IdentifierAutomaton(string input, int i)
+    {
+        LexicalIndicator li = new LexicalIndicator();
+
         int initial = i;
         int q = 0;
         bool end = false;
-        while(!end == true)
+        do
         {
-
             switch(q)
             {
                 case 0:
@@ -235,19 +244,21 @@ public static class LexicalAnalyzer
             switch (q)
             {
                 case 2:
-                    Utils.AddToken(TokenType.Identifier, Utils.GetSubString(input, initial, i));
+                    Utils.AddToken(TokenType.Identifier, initial, i, input);
+                    li.prox = i;
+                    li.error = false;
                     end = true;
                     break;
 
-                default:
-                    break;
             }
-        }
-        return i;
+        } while(end == false);
+        return li;
     }
 
-    public static int NumbersAutomaton(string input, int i)
+    public static LexicalIndicator NumbersAutomaton(string input, int i)
     {
+        LexicalIndicator li = new LexicalIndicator();
+
         int q = 0;
         int initial = i;
 
@@ -347,20 +358,26 @@ public static class LexicalAnalyzer
 
             if(q == 3)
             {
-                Utils.AddToken(TokenType.Int, Utils.GetSubString(input, initial, i));
+                Utils.AddToken(TokenType.Int, initial, i, input);
+                li.prox = i;
+                li.error = false;
                 end = true;
             }
             else if(q == 6)
             {
-                Utils.AddToken(TokenType.Float, Utils.GetSubString(input, initial, i));
+                Utils.AddToken(TokenType.Float, initial, i, input);
+                li.prox = i;
+                li.error = false;
                 end = true;
             }
         }
-        return i;
+        return li;
     }
 
-    public static int RelationalAutomaton(string input, int i)
+    public static LexicalIndicator RelationalAutomaton(string input, int i)
     {
+        LexicalIndicator li = new LexicalIndicator();
+
         int q = 0;
         int initial = i;
 
@@ -450,42 +467,58 @@ public static class LexicalAnalyzer
             switch(q)
             {
                 case 1:
-                    Utils.AddToken(TokenType.RelationalOperator, Utils.GetSubString(input, initial, i));
+                    Utils.AddToken(TokenType.RelationalOperator, initial, i, input);
+                    li.prox = i;
+                    li.error = false;
                     end = true;
                     break;
 
                 case 3:
-                    Utils.AddToken(TokenType.RelationalOperator, Utils.GetSubString(input, initial, i));
+                    Utils.AddToken(TokenType.RelationalOperator, initial, i, input);
+                    li.prox = i;
+                    li.error = false;
                     end = true;
                     break;
 
                 case 4:
-                    Utils.AddToken(TokenType.RelationalOperator, Utils.GetSubString(input, initial, i));
+                    Utils.AddToken(TokenType.RelationalOperator, initial, i, input);
+                    li.prox = i;
+                    li.error = false;
                     end = true;
                     break;
 
                 case 5:
-                    Utils.AddToken(TokenType.RelationalOperator, Utils.GetSubString(input, initial, i));
+                    Utils.AddToken(TokenType.RelationalOperator, initial, i, input);
+                    li.prox = i;
+                    li.error = false;
                     end = true;
                     break;
 
                 case 7:
-                    Utils.AddToken(TokenType.RelationalOperator, Utils.GetSubString(input, initial, i));
+                    Utils.AddToken(TokenType.RelationalOperator, initial, i, input);
+                    li.prox = i;
+                    li.error = false;
                     end = true;
                     break;
 
                 case 8:
-                    Utils.AddToken(TokenType.RelationalOperator, Utils.GetSubString(input, initial, i));
+                    Utils.AddToken(TokenType.RelationalOperator, initial, i, input);
+                    li.prox = i;
+                    li.error = false;
                     end = true;
                     break;
                 
                 case 9:
-                    Utils.AddToken(TokenType.RelationalOperator, Utils.GetSubString(input, initial, i));
+                    Utils.AddToken(TokenType.RelationalOperator, initial, i, input);
+                    li.prox = i;
+                    li.error = false;
                     end = true;
                     break;
 
                 case 10:
-                    Utils.AddToken(TokenType.RelationalOperator, Utils.GetSubString(input, initial, i));
+                    Utils.AddToken(TokenType.RelationalOperator, initial, i, input);
+                    li.prox = i;
+                    li.error = false;
                     end = true;
                     break;
 
@@ -494,8 +527,203 @@ public static class LexicalAnalyzer
             }
         }
 
-        return i;
+        return li;
     }
+
+    public static LexicalIndicator AssignmentAutomaton(string input, int i)
+    {
+        LexicalIndicator li = new LexicalIndicator();
+
+        int initial = i;
+        int q = 0;
+        bool end = false;
+
+        do
+        {
+            switch(q)
+            {
+                case 0:
+                    if(input[i] == ':')
+                    {
+                        i++;
+                        q = 1;
+                    }
+                    break;
+                
+                case 1:
+                    if(input[i] == '=')
+                    {
+                        i++;
+                        q = 2;
+                    }
+                    else
+                    {
+                        q = 3;
+                    }
+                    break;
+            }
+
+            if(i >= input.Length)
+            {
+                switch(q)
+                {
+                    case 1:
+                        q = 3;
+                        break;
+                }
+            }
+
+
+            switch(q)
+            {
+                case 2:
+                    Utils.AddToken(TokenType.Assignment, initial, i, input);
+                    li.prox = i;
+                    li.error = false;
+                    end = true;
+                    break;
+                
+                case 3:
+                    li.prox = i;
+                    li.error = true;
+                    end = true;
+                    break;
+            }
+
+        } while((end == false) && (li.error == false));
+        return li;
+    }
+
+    public static LexicalIndicator ParenthesisAutomaton(string input, int i)
+    {
+        LexicalIndicator li = new LexicalIndicator();
+
+        int initial = i;
+        int q = 0;
+        bool end = false;
+
+        do
+        {
+            switch(q)
+            {
+                case 0:
+                    if(input[i] == '(')
+                    {
+                        i++;
+                        q = 1;
+                    }
+                    else if(input[i] == ')')
+                    {
+                        i++;
+                        q = 2;
+                    }
+                    break;
+                
+            }
+
+            switch(q)
+            {
+                case 1:
+                    Utils.AddToken(TokenType.LParenthesis, initial, i, input);
+                    li.prox = i;
+                    li.error = false;
+                    end = true;
+                    break;
+                
+                case 2:
+                    Utils.AddToken(TokenType.RParenthesis, initial, i, input);
+                    li.prox = i;
+                    li.error = false;
+                    end = true;
+                    break;
+            }
+
+        } while((end == false) && (li.error == false));
+        return li;
+    }
+
+    public static LexicalIndicator OperatorAutomaton(string input, int i)
+    {
+        LexicalIndicator li = new LexicalIndicator();
+
+        int initial = i;
+        int q = 0;
+        bool end = false;
+
+        do
+        {
+            switch(q)
+            {
+                case 0:
+                    if(input[i] == '+' || input[i] == '-')
+                    {
+                        i++;
+                        q = 1;
+                    }
+                    else if(input[i] == '/' || input[i] == '*')
+                    {
+                        i++;
+                        q = 2;
+                    }
+                    break;
+                
+            }
+
+            switch(q)
+            {
+                case 1:
+                    Utils.AddToken(TokenType.AddingOperator, initial, i, input);
+                    li.prox = i;
+                    li.error = false;
+                    end = true;
+                    break;
+                
+                case 2:
+                    Utils.AddToken(TokenType.MultiplyingOperator, initial, i, input);
+                    li.prox = i;
+                    li.error = false;
+                    end = true;
+                    break;
+            }
+
+        } while((end == false) && (li.error == false));
+        return li;
+    }
+}
+
+public static class Global{
+    public static string digits = "0123456789";
+    public static string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static string sign = "+-";
+    public static string addingOperator = "+-";
+    public static string multiplyingOperator = "*/";
+    public static string relational = "=<>|&";
+    public static string parenthesis = "()";
+    public static char dot = '.';
+    public static List<Token> tokens = new List<Token>();
+}
+
+public enum TokenType
+{
+    Null = 0,
+    Identifier = 1,
+    Sign = 2,
+    MultiplyingOperator = 3,
+    AddingOperator = 4,
+    RelationalOperator = 5,
+    Int = 6,
+    Float = 7,
+    LParenthesis = 8,
+    RParenthesis = 9,
+    Assignment = 10
+}
+
+public class Token
+{
+    public TokenType type;
+    public string value = "";
+    public int initial = 0;
+    public int final = 0;
 }
 
 public static class Utils
@@ -508,13 +736,15 @@ public static class Utils
         return false;
     }
 
-    public static void AddToken(TokenType tokenType, string value)
+    public static void AddToken(TokenType tokenType, int i, int f, string input)
     {
 
         Token tk = new Token
         {
             type = tokenType,
-            value = value
+            value = GetSubString(input, i, f),
+            initial = i,
+            final = f
         };
 
         Global.tokens.Add(tk);
@@ -537,5 +767,43 @@ public static class Utils
             Console.WriteLine("Type: {0} | Value: {1}", tk.type, tk.value);
             Console.WriteLine("--------------------");
         }
+    }
+
+    public static void PrintHighlighted()
+    {
+        foreach(Token tk in Global.tokens)
+        {
+            if(tk.type == TokenType.Identifier)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if(
+                tk.type == TokenType.AddingOperator ||
+                tk.type == TokenType.MultiplyingOperator ||
+                tk.type == TokenType.RelationalOperator ||
+                tk.type == TokenType.Assignment
+            )
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else if(
+                tk.type == TokenType.Int ||
+                tk.type == TokenType.Float
+            )
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+            }
+            else if(
+                tk.type == TokenType.LParenthesis ||
+                tk.type == TokenType.RParenthesis
+            )
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+
+            Console.Write(tk.value);
+            Console.ResetColor();
+        }
+        Console.WriteLine("");
     }
 }
